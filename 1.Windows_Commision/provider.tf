76d3728085c5252a7e9cd92ad790d73aabe01775
Naveen0368 <m.naveen368@outlook.com>
2024-05-02 11:38:25 +0530

    Please enter the commit message for your changes. Lines starting

--- 1.Windows_Commision/provider.tf
+++
@@ -1,9 +0,0 @@
-provider "vsphere" {
-  user           = "your-vsphere-username"
-  password       = "your-vsphere-password"
-  vsphere_server = "your-vsphere-server"
-
-  # If you have a self-signed cert
-  allow_unverified_ssl = true
-}
-
